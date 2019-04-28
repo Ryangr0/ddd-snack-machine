de9a5e357c3ea27dabcc140dca96175296e83df6
Ryan Grippeling <R.Grippeling@hotmail.com>
2019-04-28 09:03:38 +0200

    Test that event is thrown

--- tests/Model/SnackMachineTest.php
+++ tests/Model/SnackMachineTest.php
@@ -2,8 +2,10 @@
 
 namespace SnackMachine\Tests\Model;
 
+use Prooph\EventSourcing\AggregateChanged;
 use Ramsey\Uuid\Exception\InvalidUuidStringException;
 use Ramsey\Uuid\Uuid;
+use SnackMachine\Events\SnackMachineTurnedOn;
 use SnackMachine\Model\SnackMachine;
 use SnackMachine\Model\SnackMachine\SnackMachineId;
 use SnackMachine\Tests\AggregateTestCase;
@@ -29,5 +31,23 @@ class SnackMachineTest extends AggregateTestCase
             SnackMachine::class,
             SnackMachine::turnOn($snackMachineId->reveal())
         );
+    }
+
+    public function test_turns_machine_on()
+    {
+        $snackMachineId = $this->prophesize(SnackMachineId::class);
+        $snackMachineId->toString()->willReturn(Uuid::uuid4()->toString());
+
+        $snackMachine = SnackMachine::turnOn($snackMachineId->reveal());
+
+        /** @var AggregateChanged[] $events */
+        $events = $this->popRecordedEvents($snackMachine);
+
+        $this->assertCount(1, $events);
+
+        /** @var \SnackMachine\Events\SnackMachineTurnedOn $event */
+        $event = $events[0];
+
+        $this->assertSame(SnackMachineTurnedOn::class, $event->messageName());
     }
 }
